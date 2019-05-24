9760b04aee267370e7c25635460d00bde51372ce
sacha <sacha.bloch.etu@univ-lille.fr>
2019-05-24 11:54:07 +0200

    Homepage carousel + jumbtron find

--- Website/alytus-front/src/app/home/home.component.ts
+++ Website/alytus-front/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {News} from "../models/news";
+import {NewsService} from "../news.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  imagePath = environment.imagePath;
+  dateNow: Date;
+  week: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  month: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  formattedDate: string;
+  allNews: Observable<News[]>;
+  lastNewsOf: News[]= [];
+  sports: boolean;
+  buildings: boolean;
+  politics: boolean;
+  business: boolean;
+  index: number;
+
+  constructor(private newsService: NewsService) { }
 
   ngOnInit() {
+    this.sports = this.buildings = this.politics = this.business = false;
+    this.index = 0;
+    this.dateNow = new Date();
+    this.formattedDate = this.getFormattedDate();
+    console.log(this.formattedDate);
+    this.allNews = this.newsService.getNews();
+    this.allNews.forEach(x => x.forEach((news) => this.getLastNewsOf(news)));
+    console.log(this.lastNewsOf);
   }
+
+  getFormattedDate(){
+    let dayNumber: number = this.dateNow.getUTCDate();
+    let day: number = this.dateNow.getUTCDay();
+    let month: number = this.dateNow.getUTCMonth();
+    return `${this.week[day]}, ${dayNumber} ${this.month[month]}`;
+  }
+
+  getLastNewsOf(news: News) {
+    if(this.index < 4) {
+      if(news.keywords[0] == "Sports" && !this.sports) {
+        this.lastNewsOf[this.index] = news;
+        this.index++;
+        this.sports = true;
+      }
+      if(news.keywords[0] == "Buildings" && !this.buildings) {
+        this.lastNewsOf[this.index] = news;
+        this.index++;
+        this.buildings = true;
+      }
+      if(news.keywords[0] == "Politics" && !this.politics) {
+        this.lastNewsOf[this.index] = news;
+        this.index++;
+        this.politics = true;
+      }
+      if(news.keywords[0] == "Business" && !this.business) {
+        this.lastNewsOf[this.index] = news;
+        this.index++;
+        this.business = true;
+      }
+    }
+  }
+
 
 }
