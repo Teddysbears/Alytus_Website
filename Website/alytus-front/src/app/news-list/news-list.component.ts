02374dbc30188afa934e4f25b22260cf61fecb60
sacha <sacha.bloch.etu@univ-lille.fr>
2019-05-28 13:31:37 +0200

    no more bug with news page an news page done 80% footer work fine for all pages now, redirection bug corrected

--- Website/alytus-front/src/app/news-list/news-list.component.ts
+++ Website/alytus-front/src/app/news-list/news-list.component.ts
@@ -13,12 +13,35 @@ import { environment } from "../../environments/environment";
 export class NewsListComponent implements OnInit {
   newsList$: Observable<News[]>;
   imagePath = environment.imagePath;
-
+  politicsNews: News = null;
+  buildingsNews: News = null;
+  sportsNews: News = null;
+  businessNews: News = null;
 
   constructor(private newsService: NewsService) { }
 
   ngOnInit() {
     this.newsList$ = this.newsService.getNews();
+    this.newsList$.subscribe(news => this.newsTypeCreator(news))
+  }
+
+  private newsTypeCreator(news: News[]) {
+    let activeNews: News;
+    news.forEach( data => {
+      activeNews = data;
+      if(activeNews.keywords[0] == 'Politics' && this.politicsNews == null) {
+        this.politicsNews = activeNews;
+      }
+      if(activeNews.keywords[0] == 'Buildings' && this.buildingsNews == null) {
+        this.buildingsNews = activeNews;
+      }
+      if(activeNews.keywords[0] == 'Sports' && this.sportsNews == null) {
+        this.sportsNews = activeNews;
+      }
+      if(activeNews.keywords[0] == 'Business' && this.businessNews == null) {
+        this.businessNews = activeNews;
+      }
+    });
   }
 
 }
